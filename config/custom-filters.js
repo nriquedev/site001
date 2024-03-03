93507fb691835cfaaff35dc993390aad882e8721
Jestemrique <Jestemrique@users.noreply.github.com>
2024-03-03 18:02:26 +0000

    deploy: 6649c8fae0092e2f152f914a379d5d286a91b050

--- config/custom-filters.js
+++
@@ -1,9 +0,0 @@
-const { DateTime } = require("luxon");
-
-module.exports = eleventyConfig => {
-    //Date filters
-    eleventyConfig.addFilter("postDate", dateObj => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-    });
-}
-
